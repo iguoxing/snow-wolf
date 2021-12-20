30fad0c0186ceeab01278c1ee5f61af3e099f3d5
ardenzhao <iguoxing@163.com>
2021-12-20 13:18:49 +0800

    Create 10-链表中间节点.js

---
+++ 3、algorithm/10-链表中间节点.js
@@ -0,0 +1,18 @@
+// Copyright 2021 zhaoarden
+// 
+// https://leetcode-cn.com/problems/middle-of-the-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var middleNode = function(head) {
+
+};
