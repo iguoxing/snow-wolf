6afd7aca8aecb0ec3864687ee2c96615c274c378
ardenzhao <iguoxing@163.com>
2022-03-28 19:41:00 +0800

    算法

---
+++ 3、algorithm/4-stack/1-剑指 Offer 09. 用两个栈实现队列.js
@@ -0,0 +1,42 @@
+/*
+ * @Author: ArdenZhao
+ * @Date: 2022-03-28 18:30:11
+ * @LastEditTime: 2022-03-28 18:31:03
+ * @FilePath: /snow-wolf/3、algorithm/4-stack/1-剑指 Offer 09. 用两个栈实现队列.js
+ * @Description: file information
+ * https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+ * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ * 示例 1：
+
+输入：
+["CQueue","appendTail","deleteHead","deleteHead"]
+[[],[3],[],[]]
+输出：[null,null,3,-1]
+ */
+
+var CQueue = function () {
+
+};
+
+/**
+ * @param {number} value
+ * @return {void}
+ */
+CQueue.prototype.appendTail = function (value) {
+
+};
+
+/**
+ * @return {number}
+ */
+CQueue.prototype.deleteHead = function () {
+
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * var obj = new CQueue()
+ * obj.appendTail(value)
+ * var param_2 = obj.deleteHead()
+ */
--- Readme.md
+++ Readme.md
@@ -1,1 +1,7 @@
-### Arden'FE
+# Arden'FE
+
+## 剑指offer
+
+https://leetcode-cn.com/study-plan/lcof/?progress=7zj4ms2
+
+> 每天2-3题
